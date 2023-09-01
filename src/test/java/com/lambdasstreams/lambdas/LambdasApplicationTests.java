b71819a5ea07124bc1a4e8e489d2a8f7e9da9a30
Ruf Rojas <ruf_rm@hotmail.com>
2023-08-31 18:14:59 -0600

    initial commit

---
+++ src/test/java/com/lambdasstreams/lambdas/LambdasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambdasstreams.lambdas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambdasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
