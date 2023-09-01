c87b0e582e1c080a1e5cb9855d4c39e88a6291f4
rrojasM <ruf_rm@hotmail.com>
2023-08-31 18:53:37 -0600

    firts changes

--- src/main/java/com/lambdasstreams/lambdas/LambdasApplication.java
+++ src/main/java/com/lambdasstreams/lambdas/LambdasApplication.java
@@ -8,6 +8,7 @@ public class LambdasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LambdasApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
