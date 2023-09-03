e4c3c77c438117fb4ce35b09debc74bdc38d983f
rrojasM <ruf_rm@hotmail.com>
2023-09-02 22:51:05 -0600

    Implementando clase anonima

--- src/main/java/com/lambdasstreams/lambdas/LambdasApplication.java
+++ src/main/java/com/lambdasstreams/lambdas/LambdasApplication.java
@@ -4,6 +4,8 @@ import com.lambdasstreams.lambdas.interfaces.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class LambdasApplication {
 
@@ -20,6 +22,26 @@ public class LambdasApplication {
 
 		System.out.println(employeeDB.getById(12L).toString());
 		System.out.println(productDB.getById(30L).toString());
+
+
+		System.out.println(Product.class.getName());
+		System.out.println(Employee.class.getName());
+
+		//IMPLEMENTANDO CLASE ANONIMA
+		DataService<String> dbAnonima = new DataService<String>() {
+			@Override
+			public String getById(Long id) {
+				return null;
+			}
+
+			@Override
+			public List<String> getAllRecords() {
+				return null;
+			}
+		};
+
+		System.out.println(dbAnonima.getClass().getName());
+
 	}
 
 }
